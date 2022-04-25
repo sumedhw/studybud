c74ca71ae61e4e3df413469e33123576fbc86432
sumedh <sumedhw71@gmail.com>
2022-04-26 01:35:40 +0530

    added models and views

--- base/views.py
+++ base/views.py
@@ -1,3 +1,4 @@
+from multiprocessing import context
 from django.shortcuts import render
 from . models import Room
 
@@ -7,7 +8,9 @@ from . models import Room
 def home(request):
     rooms = Room.objects.all()
     context = { 'rooms' : rooms }
-    return render(request,'base/home.html')
+    return render(request,'base/home.html', context)
 
-def room(request):
-    return render(request, 'base/room.html')
+def room(request, pk):
+    room = Room.objects.get(id=pk)
+    context = {'room': room}
+    return render(request, 'base/room.html', context)
