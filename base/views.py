93e484499b565a140c7883d64548de1b64280808
sumedh <sumedhw71@gmail.com>
2022-04-28 00:14:52 +0530

    template updated

--- base/views.py
+++ base/views.py
@@ -14,3 +14,8 @@ def room(request, pk):
     room = Room.objects.get(id=pk)
     context = {'room': room}
     return render(request, 'base/room.html', context)
+
+def createRoom(request):
+    
+    context = {}
+    return render(request, 'base/room_form.html', context)
