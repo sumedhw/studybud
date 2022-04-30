6a22f7e19165d95e85730f1baf10254a8b1914b2
sumedh <sumedhw71@gmail.com>
2022-05-01 01:18:08 +0530

    UI changes and rooms crud operation added

--- base/views.py
+++ base/views.py
@@ -1,14 +1,27 @@
 from multiprocessing import context
 from django.shortcuts import render, redirect
-from . models import Room
+from django.db.models import Q
+
+from . models import Room, Topic
 from .forms import RoomForm
 
 # Create your views here.
 
 
 def home(request):
-    rooms = Room.objects.all()
-    context = { 'rooms' : rooms }
+    q = request.GET.get('q') if request.GET.get('q') != None else ''
+
+    rooms = Room.objects.filter(
+        Q(topic__name__icontains=q) |
+        Q(name__icontains=q) |
+        Q(description__icontains=q)
+    )
+
+    topics = Topic.objects.all()
+    room_count = rooms.count()
+    
+    context = { 'rooms' : rooms, 'topics' : topics, 'room_count': room_count }
+
     return render(request,'base/home.html', context)
 
 def room(request, pk):
@@ -40,3 +53,11 @@ def updateRoom(request, pk):
 
     context = {'form': form}
     return render(request, 'base/room_form.html', context)
+
+
+def deleteRoom(request, pk):
+    room = Room.objects.get(id=pk)
+    if request.method == 'POST':
+        room.delete()
+        return redirect('home')    
+    return render(request, 'base/delete.html', {'obj':room})
