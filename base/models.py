90b5793702adef27421430986ba419be6f6ddd08
sumedh <sumedhw71@gmail.com>
2022-05-07 23:44:26 +0530

    room comments functionality added

--- base/models.py
+++ base/models.py
@@ -9,13 +9,12 @@ class Topic(models.Model):
     def __str__(self) -> str:
         return self.name
 
-
 class Room(models.Model):
     host = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length=200)
     description = models.TextField(null=True, blank=True)
-    # participants
+    participants = models.ManyToManyField(User, related_name='participants', blank=True)
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
 
--- base/urls.py
+++ base/urls.py
@@ -11,5 +11,7 @@ urlpatterns = [
 
     path('create-room/', views.createRoom, name="create-room"),
     path('update-room/<str:pk>', views.updateRoom, name="update-room"),
-    path('delete-room/<str:pk>', views.deleteRoom, name="delete-room")
+    path('delete-room/<str:pk>', views.deleteRoom, name="delete-room"),
+
+    path('delete-message/<str:pk>', views.deleteMessage, name="delete-message")
 ]
