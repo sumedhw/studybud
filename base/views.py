ce9a422c67d84d2a96d52fc64e6f69753b27e53b
sumedh <sumedhw71@gmail.com>
2022-05-10 23:57:40 +0530

    feed activity functionality adde

--- base/views.py
+++ base/views.py
@@ -79,8 +79,10 @@ def home(request):
 
     topics = Topic.objects.all()
     room_count = rooms.count()
-    
-    context = { 'rooms' : rooms, 'topics' : topics, 'room_count': room_count }
+    room_messages = Message.objects.all()
+
+
+    context = { 'rooms' : rooms, 'topics' : topics, 'room_count': room_count, 'room_messages' : room_messages }
 
     return render(request,'base/home.html', context)
 
