db004b4436148c831bc91160477654c719012c1a
sumedh <sumedhw71@gmail.com>
2022-05-15 00:23:59 +0530

    UI components are added

--- base/views.py
+++ base/views.py
@@ -79,7 +79,7 @@ def home(request):
 
     topics = Topic.objects.all()
     room_count = rooms.count()
-    room_messages = Message.objects.all()
+    room_messages = Message.objects.filter(Q(room__topic__name__icontains=q))
 
 
     context = { 'rooms' : rooms, 'topics' : topics, 'room_count': room_count, 'room_messages' : room_messages }
@@ -102,6 +102,12 @@ def room(request, pk):
 
     context = {'room': room, 'room_messages': room_messages, 'participants':participants}
     return render(request, 'base/room.html', context)
+
+
+def userProfile(request):
+    context = {}
+    return render(request, 'base/profile.html', context)
+
 
 @login_required(login_url="login")
 def createRoom(request):
